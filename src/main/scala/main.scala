152bad169d5020b941f2db1cd503d5de85ed5b22
jBosak <jbosak98@gmail.com>
2020-04-04 15:28:35 +0200

    refactored imports in main and loadSudokus

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,17 @@
-
+import tools.loadSudokus.loadSudokus
+import tools.printSudoku.printSudoku
 
 object main {
+
   def main(args: Array[String]): Unit = {
     val filename = "src/main/resources/ai-lab2-2020-dane/Sudoku2.csv"
-    val sudokus = loadSudokus.loadSudokus(filename);
-    println(sudokus)
-    sudokus.foreach(_.rowSudoku.foreach(println))
+    val sudokus = loadSudokus(filename);
+    //    println(sudokus)
+    //    sudokus.foreach(_.rowSudoku.foreach(println))
+    printSudoku(sudokus.head)
 
   }
 
 
 }
+
---
+++ src/main/scala/tools/loadSudokus.scala
@@ -0,0 +1,33 @@
+package tools
+
+import model.{Sudoku, ValueSudoku}
+
+import scala.io.Source.fromFile
+
+object loadSudokus {
+  def loadSudokus(filename: String) = {
+    val bufferedSource = fromFile(filename)
+    val sudokusIterator = for {fileRow <- bufferedSource.getLines
+                               sudoku = _loadSudoku(fileRow)
+                               if fileRow.exists(_.isDigit) || fileRow.exists(_ == '.')
+                               } yield sudoku
+    val sudokus = sudokusIterator.toList
+    bufferedSource.close()
+    sudokus
+
+  }
+
+  private def _loadSudoku(line: String): Sudoku = {
+    val values = line.split(';')(2)
+    val sudokuValues = for {sudokuNumber <- values
+                            singleValue = _toInt(sudokuNumber)
+                            valueSudoku = new ValueSudoku(singleValue, singleValue.isDefined)
+                            } yield valueSudoku
+    Sudoku(sudokuValues.toArray)
+  }
+
+  private def _toInt(i: Char): Option[Int] = i match {
+    case i if i.isDigit => Option(i.asDigit)
+    case _ => Option.empty[Int]
+  }
+}
